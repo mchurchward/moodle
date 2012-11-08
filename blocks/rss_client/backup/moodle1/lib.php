055612fcf5c0d52aae029a4959134ef11e8283a3
Mike Churchward <mike@remote-learner.net>
2012-11-08 12:07:12 -0500

    MDL-32880 - An example of how block restore would work for instances with special data.

--- blocks/rss_client/backup/moodle1/lib.php
+++ blocks/rss_client/backup/moodle1/lib.php
@@ -14,27 +14,57 @@ defined('MOODLE_INTERNAL') || die();
  * Block conversion handler for rss_client
  */
 class moodle1_block_rss_client_handler extends moodle1_block_handler {
+    /** @var int instanceid */
+    protected $instanceid = null;
+
+    /** @var int contextid */
+    protected $contextid = null;
+
     public function get_paths() {
         $paths = parent::get_paths();
-        $paths[] = new convert_path('rss_client_feeds', "/MOODLE_BACKUP/COURSE/BLOCKS/BLOCK/RSS_CLIENT/FEEDS/FEED");
+        $paths[] = new convert_path('rss_client_feeds', "/MOODLE_BACKUP/COURSE/BLOCKS/BLOCK/RSS_CLIENT/FEEDS");
+        $paths[] = new convert_path('rss_client_feed', "/MOODLE_BACKUP/COURSE/BLOCKS/BLOCK/RSS_CLIENT/FEEDS/FEED");
         return $paths;
     }
 
-    public function process_rss_client_feeds(array $data) {
-        $instanceid = $data['id'];
-		$contextid = $this->converter->get_contextid(CONTEXT_BLOCK, $data['id']);
+    public function process_block(array $data) {
+        $this->instanceid = $data['id'];
+		$this->contextid = $this->converter->get_contextid(CONTEXT_BLOCK, $data['id']);
+		return parent::process_block($data);
+    }
 
-		// Moodle 1.9 backups do not include sufficient data to restore feeds, so we need an empty shell rss_client.xml
-		// for the restore process to find
-		$this->open_xml_writer("course/blocks/{$data['name']}_{$instanceid}/rss_client.xml");
-        $this->xmlwriter->begin_tag('block', array('id' => $instanceid, 'contextid' => $contextid, 'blockname' => 'rss_client'));
-        $this->xmlwriter->begin_tag('rss_client', array('id' => $instanceid));
-        $this->xmlwriter->full_tag('feeds', '');
-        $this->xmlwriter->full_tag('feed', '');
+    /**
+     * This is executed when the parser reaches the <FEEDS> opening element
+     */
+    public function on_rss_client_feeds_start() {
+		$this->open_xml_writer("course/blocks/{$this->pluginname}_{$this->instanceid}/rss_client.xml");
+        $this->xmlwriter->begin_tag('block', array('id' => $this->instanceid, 'contextid' => $this->contextid, 'blockname' => 'rss_client'));
+        $this->xmlwriter->begin_tag('rss_client', array('id' => $this->instanceid));
+        $this->xmlwriter->begin_tag('feeds');
+    }
+
+    /**
+     * This is executed when the parser reaches the closing </FEEDS> element
+     */
+    public function on_rss_client_feeds_end() {
+        $this->xmlwriter->end_tag('feeds');
         $this->xmlwriter->end_tag('rss_client');
         $this->xmlwriter->end_tag('block');
         $this->close_xml_writer();
+    }
 
+    public function process_rss_client_feeds(array $data) {
+        return $data;
+	}
+
+    public function process_rss_client_feed(array $data) {
+        $this->xmlwriter->begin_tag('feed', array('id' => $data['id']));
+        foreach ($data as $field => $value) {
+            if ($field <> 'id') {
+                $this->xmlwriter->full_tag($field, $value);
+            }
+        }
+        $this->xmlwriter->end_tag('feed');
         return $data;
 	}
 }
